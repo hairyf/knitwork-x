54ae87ff62e2c01151b880505525b35769f59e09
hairyf <wwu710632@gmail.com>
2026-02-02 11:54:56 +0800

    chore: enhance knitwork-x documentation and configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default antfu(
   {
     type: 'lib',
     pnpm: true,
-    ignores: ['README.md', 'docs/**/*.md'],
+    ignores: ['README.md', 'docs/**/*.md', 'skills/**/*.md'],
     rules: {
       'no-template-curly-in-string': 'off',
       'ts/explicit-function-return-type': 'off',
